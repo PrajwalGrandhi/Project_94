e45fcb95a9caf1fa79b14d3a7758fcfff754bb75
Prajwal Shreshta Grandhi <70463518+PrajwalGrandhi@users.noreply.github.com>
2021-06-18 17:15:14 +0530

    Add files via upload

---
+++ screens/PomodoroTimer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, Text, View ,TextInput,TouchableOpacity, Alert,Modal, ScrollView, KeyboardAvoidingView} from 'react-native';
+
+export default class PomodoroTimer extends React.Component{
+  render(){
+    return (
+        <View>
+<Text>pomodoro</Text>
+        </View>
+    );
+  }
+}
